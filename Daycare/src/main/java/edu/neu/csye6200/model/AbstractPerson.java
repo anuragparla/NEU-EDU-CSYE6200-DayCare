413afaab6b4c685f51a69b987e77c6a4ba8c2cb0
anuragparla <anuragparla@gmail.com>
2021-12-08 18:44:38 -0500

    AbstractPerson class

---
+++ Daycare/src/main/java/edu/neu/csye6200/model/AbstractPerson.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.neu.csye6200.model;
+
+/**
+ *
+ * @author anuragparla
+ */
+public abstract class AbstractPerson {
+    public int age;
+    public String name;
+    public abstract void setage(int age);
+    public abstract int getAge(int age);
+    public abstract void setName(String name);
+    public abstract String getName ();
+    
+}
