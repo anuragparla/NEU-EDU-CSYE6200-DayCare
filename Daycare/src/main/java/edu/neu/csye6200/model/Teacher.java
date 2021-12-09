81e75a65410baf72b7647cb8f4329217c1027525
anuragparla <anuragparla@gmail.com>
2021-12-08 20:00:16 -0500

    Teacher class

---
+++ Daycare/src/main/java/edu/neu/csye6200/model/Teacher.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.neu.csye6200.model;
+
+/**
+ *
+ * @author anuragparla
+ */
+public class Teacher {
+    private int credits;
+    public Teacher(int age, String name, int credits) {
+        super();
+        
+    }
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+    public int getCredits() {
+        return credits;
+    }
+    
+}
