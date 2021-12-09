4cacb798a6b59eb5af5e896a7505e3bd834f6583
anuragparla <anuragparla@gmail.com>
2021-12-09 00:01:16 -0500

    reduced code duplicacy

--- Daycare/src/main/java/edu/neu/csye6200/model/Teacher.java
+++ Daycare/src/main/java/edu/neu/csye6200/model/Teacher.java
@@ -14,25 +14,6 @@ public class Teacher extends Person{
         super(age, name);
         this.credits = credits;
     }
-    @Override
-    public void setage(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public int getAge(int age) {
-        return age;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
     
     public void setCredits(int credits) {
         this.credits = credits;
