11c0d884cd53e3777665316a9696168b8e6fc093
anuragparla <anuragparla@gmail.com>
2021-12-11 15:59:52 -0500

    vaccine interface created

---
+++ Daycare/src/main/java/edu/neu/csye6200/model/Dummyvaccine.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.neu.csye6200.model;
+
+/**
+ *
+ * @author anuragparla
+ * This class consists of vaccine name and description. 
+ */
+public class Dummyvaccine {
+    private String name;
+    private String description;
+
+    public Dummyvaccine(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+}
--- Daycare/src/main/java/edu/neu/csye6200/model/Vaccine.java
+++ Daycare/src/main/java/edu/neu/csye6200/model/Vaccine.java
@@ -1,38 +1,15 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package edu.neu.csye6200.model;
 
 /**
  *
  * @author anuragparla
- * This class consists of vaccine name and description
  */
-public class Vaccine {
-    private String name;
-    private String description;
-
-    public Vaccine(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+public interface Vaccine {
+    public void add();
     
     
 }
